5dab36a42c61f18b743f4eb7eff1692bab823e24
Patryk Gneba <gnebapatryk@gmail.com>
2022-12-10 21:50:27 +0100

    Add docs

--- eCommerce.Application/Common/Mappings/IMappings.cs
+++ eCommerce.Application/Common/Mappings/IMappings.cs
@@ -2,16 +2,29 @@
 
 namespace eCommerce.Application.Common.Mappings
 {
+    /// <summary>
+    /// Map from given type to current
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public interface IMapFrom<TEntity> where TEntity : class
     {
         void Mapping(Profile profile) => profile.CreateMap(typeof(TEntity), GetType());
     }
 
+    /// <summary>
+    /// Map from current type to specified type
+    /// </summary>
+    /// <typeparam name="Type"></typeparam>
     public interface IMapTo<Type> where Type : class
     {
         void Mapping(Profile profile) => profile.CreateMap(GetType(), typeof(Type));
     }
 
+    /// <summary>
+    /// Map mutually from type1 to type 2 and reverse
+    /// </summary>
+    /// <typeparam name="Type1"></typeparam>
+    /// <typeparam name="Type2"></typeparam>
     public interface IMapMutually<Type1, Type2>
         where Type1 : class
         where Type2 : class
--- eCommerce.Application/Contracts/Persistence/Repositories/Base/IBaseRepository.cs
+++ eCommerce.Application/Contracts/Persistence/Repositories/Base/IBaseRepository.cs
@@ -4,24 +4,84 @@ namespace eCommerce.Application.Contracts.Persistence.Repositories.Base
 {
     public interface IBaseRepository<TEntity> where TEntity : class
     {
+        /// <summary>
+        /// Add single element
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Add multiple elements
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
         Task<IEnumerable<TEntity>> AddRangeAsync(IEnumerable<TEntity> entities);
 
+        /// <summary>
+        /// Remove specific element
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task RemoveAsync(TEntity entity, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Update specific element
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
 
-        Task<int> CountAsync();
+        /// <summary>
+        /// Returns count of elemts
+        /// </summary>
+        /// <returns></returns>
+        Task<int> CountAsync(CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Returns count of elemts that fulfill condition
+        /// </summary>
+        /// <returns></returns>
+        Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Returns element that fulfill predicate
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Returns all elements that fulfill predicate
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<TEntity?> FindAllAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Returns dbset as IQuerable
+        /// </summary>
+        /// <returns></returns>
         Task<IQueryable<TEntity>> AsIQuerable();
 
+        /// <summary>
+        /// Check if given field is unique
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<bool> IsUnique(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Save DbContextChanges
+        /// </summary>
+        /// <param name="cancellation"></param>
+        /// <returns></returns>
         Task<bool> SaveChangesAsync(CancellationToken cancellation);
     }
 }
