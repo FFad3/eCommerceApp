7d78340fb1b5a65cbc4ea9affe5e3f6346a42ece
Patryk Gneba <gnebapatryk@gmail.com>
2022-12-10 21:42:05 +0100

    Add create command validator

---
+++ eCommerce.Application/Features/Commands/CreateCategoryCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using eCommerce.Application.Contracts.Persistence.Repositories;
+using FluentValidation;
+
+namespace eCommerce.Application.Features.Commands
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+        private readonly ICategoryRepository _repo;
+
+        public CreateCategoryCommandValidator(ICategoryRepository repo)
+        {
+            _repo = repo;
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required")
+                .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters")
+                .MustAsync(IsNameUnique);
+        }
+
+        private async Task<bool> IsNameUnique(string name, CancellationToken cancellationToken) =>
+            await _repo.IsUnique(x => x.Name == name, cancellationToken);
+    }
+}
