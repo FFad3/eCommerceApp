56b4bf5927268bbe331f8d716c9000121360d1b0
Patryk Gneba <gnebapatryk@gmail.com>
2022-12-12 19:29:59 +0100

    Add pipline configuration

--- eCommerce.Application/ApplicationConfigureServices.cs
+++ eCommerce.Application/ApplicationConfigureServices.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using eCommerce.Application.Common.Behaviours;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -15,8 +16,12 @@ namespace eCommerce.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             var asm = Assembly.GetExecutingAssembly();
-            services.AddAutoMapper(asm);
             services.AddMediatR(asm);
+            //Pipeline config
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AppLoggingBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehaviour<,>));
+            //
+            services.AddAutoMapper(asm);
             services.AddValidatorsFromAssembly(asm);
             return services;
         }
