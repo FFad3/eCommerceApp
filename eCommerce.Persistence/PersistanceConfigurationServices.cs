9c57fb5f090aeb9e87f3cb64545b41a6e2345444
Patryk Gneba <gnebapatryk@gmail.com>
2022-12-14 14:09:26 +0100

    Persistance services configuration

---
+++ eCommerce.Persistence/PersistanceConfigurationServices.cs
@@ -0,0 +1,34 @@
+﻿using eCommerce.Application.Contracts.Persistence.Repositories;
+using eCommerce.Persistence.Data;
+using eCommerce.Persistence.Interceptors;
+using eCommerce.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Persistence
+{
+    public static class PersistanceConfigurationServices
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region dbContext
+
+            services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString(""));
+            });
+
+            #endregion dbContext
+
+            #region Repos
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            #endregion Repos
+
+            return services;
+        }
+    }
+}
