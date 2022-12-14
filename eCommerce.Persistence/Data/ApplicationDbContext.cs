ef2690997623337815a934b3557ec29ed1eb289a
Patryk Gneba <gnebapatryk@gmail.com>
2022-12-14 13:36:14 +0100

    Add dbContext and SaveChanges interceptor

---
+++ eCommerce.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using eCommerce.Domain.Entities;
+using eCommerce.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace eCommerce.Persistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,
+            AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor)
+            : base(options)
+        {
+            _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        #region DbSets
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        #endregion DbSets
+    }
+}
