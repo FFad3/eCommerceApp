6fa0e53fb6f888f9e21647dd2ebd6ac122c361da
Patryk Gneba <gnebapatryk@gmail.com>
2022-12-17 21:41:32 +0100

    WIP

---
+++ eCommerce.Api/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using eCommerce.Application.Features.Commands;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController(IMediator mediator, ILogger<CategoryController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(CreateCategoryCommand command)
+        {
+            _logger.LogInformation("halko");
+            try
+            {
+                var result = await _mediator.Send(command);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
