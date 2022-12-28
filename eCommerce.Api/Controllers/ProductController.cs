8630d3d7833982dde6baece9f3a3418d3a6e94e3
Patryk Gneba <gnebapatryk@gmail.com>
2022-12-29 00:26:47 +0100

    Add Product controller implementation

---
+++ eCommerce.Api/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using eCommerce.Application.Features.Commands;
+using eCommerce.Application.Features.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace eCommerce.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<CategoryController> _logger;
+
+        public ProductController(IMediator mediator, ILogger<CategoryController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Summary = "Adds new product")]
+        public async Task<ActionResult> Post(CreateProductCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Get produucts page")]
+        public async Task<ActionResult> Get([FromQuery] GetProductPageQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
