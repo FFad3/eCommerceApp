5215d17a6e31af8796defac3c9e6d9eed10ff5d0
Patryk Gneba <gnebapatryk@gmail.com>
2023-01-05 13:01:10 +0100

    Add EntityTypeDescriptor & refactor

--- eCommerce.Application/Common/Extensions/IQuerableSortingExtension.cs
+++ eCommerce.Application/Common/Extensions/IQuerableSortingExtension.cs
@@ -1,27 +1,19 @@
-﻿using eCommerce.Domain.Common;
-using System.Linq.Dynamic.Core;
+﻿using System.Linq.Dynamic.Core;
 
 namespace eCommerce.Application.Common.Extensions
 {
-    public static class IQuerableSortingExtension
+    public class EntityTypeDescriptor
     {
-        public static IQueryable<T> ApplySort<T>(this IQueryable<T> source, string? sortStr)
+        public static readonly IEnumerable<string> ForbiddenSortOptions = new HashSet<string> { "CreatedBy", "CreatedDate", "LastModifiedBy", "LastModifiedDate", "IsRemoved" };
+        public HashSet<string> SortOptions { get; } = new HashSet<string>();
+
+        public EntityTypeDescriptor(Type entity)
         {
-            var filters = GetFilters(typeof(T));
+            SortOptions = GetSortOptions(entity);
+        }
 
-            if (source is null)
-            {
-                throw new ArgumentNullException(nameof(source), "Data source is empty");
-            }
-            if (filters is null)
-            {
-                throw new ArgumentNullException(nameof(filters), "no avaiable filters");
-            }
-            if (string.IsNullOrEmpty(sortStr))
-            {
-                return source.OrderBy("Id");
-            }
-
+        public string GetSortExpression(string sortStr)
+        {
             var sortStringArray = sortStr.Split(',');
 
             string sortExpression = string.Empty;
@@ -31,34 +23,57 @@ namespace eCommerce.Application.Common.Extensions
                 if (sortOption.StartsWith("-"))
                 {
                     var prop = sortOption.Remove(0, 1);
-                    if (filters.Contains(prop))
+                    if (SortOptions.Contains(prop))
                         sortExpression += prop + " DESC,";
                 }
                 else
                 {
-                    if (filters.Contains(sortOption))
+                    if (SortOptions.Contains(sortOption))
                         sortExpression += sortOption + " ASC,";
                 }
             }
+            return sortExpression;
+        }
+
+        private static HashSet<string> GetSortOptions(Type type)
+        {
+            var result = type.GetProperties()
+                            .Where(x => !ForbiddenSortOptions.Contains(x.Name))
+                            .Select(x => x.Name)
+                            .ToHashSet();
+
+            return result ?? new HashSet<string>();
+        }
+    }
+
+    public static class IQuerableSortingExtension
+    {
+        public static IQueryable<T> ApplySort<T>(this IQueryable<T> source, string? sortStr)
+        {
+            var descriptor = new EntityTypeDescriptor(typeof(T));
+
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source), "Data source is empty");
+            }
+            if (descriptor.SortOptions is null)
+            {
+                throw new ArgumentNullException(nameof(descriptor.SortOptions), "no Sort options avaiable");
+            }
+            if (string.IsNullOrEmpty(sortStr))
+            {
+                return source.OrderBy("Id");
+            }
+
+            var sortExpression = descriptor.GetSortExpression(sortStr);
 
             if (!string.IsNullOrWhiteSpace(sortExpression))
             {
-                source.OrderBy("Name DESC");
                 // Note: system.linq.dynamic NuGet package is required here to operate OrderBy on string
                 source = source.OrderBy(sortExpression.Remove(sortExpression.Length - 1));
             }
 
             return source;
-        }
-
-        private static HashSet<string>? GetFilters(Type type)
-        {
-            var result = type.GetProperties()
-                            .Where(x => typeof(EntityBase).GetProperty(x.Name) is null || x.Name == "Id")
-                            .Select(x => x.Name)
-                            .ToHashSet();
-
-            return result;
         }
     }
 }
